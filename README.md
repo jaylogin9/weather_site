d27fa82695ce51813dc44cc2ebf903ddb550351f
unknown <jaylogin9@gmail.com>
2023-01-23 00:42:27 +0530

    Weather Project

---
+++ README.md
@@ -0,0 +1,28 @@
+Weather App
+Check the current weather on any city on the planet.
+
+Features
+User's ability to search cities.
+
+Temperatures.
+
+Wind speed.
+
+Error handling.
+
+Installation
+git clone https://github.com/jaylogin9/weather
+
+cd weather-app
+
+npm install
+
+Log-in to Openweathermap.com
+
+Create an API key
+
+cp .env.example .env.local
+
+Paste API key for OPENWEATHER_API_KEY
+
+npm run dev
---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const requests = require("requests"); // <-- typo error, should be "request" not "requests"
+const path = require("path");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const template = path.join(__dirname, "/templates");
+app.set("views", template);
+
+app.get("/", (req, res) => {
+  res.render("index.hbs");
+});
+
+  // Render the Display.hbs Page.
+app.post("/info", async (req, res) => {
+  let city = req.body.nameCity;
+  const apiKey =process.env.API;
+  // fetch the response from the link.
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&&lang=hi&appid=${apiKey}`;
+  requests(url)
+    .on("data", (chunk) => {
+      const jsonData = JSON.parse(chunk);
+      //console.log(jsonData);
+      res.render("display.hbs", { data: jsonData });
+    })
+
+    .on("end", function (err) {
+      if (err) return console.log("connection closed due to errors", err);
+      
+    });
+});
+
+app.listen(7000, () => {
+  console.log("Server Start");
+});
